2de7e7e7481384581229c3e8cca127ca4524a69b
0kareem0 <kimo123kimo321@gmail.com>
2025-01-05 16:51:18 +0200

    Added-Strategy-Pattern

--- oop.js
+++ oop.js
@@ -1,23 +1,55 @@
+// Discount Strategies
+class DiscountStrategy {
+    applyDiscount(price) {
+        return price; // No discount by default
+    }
+}
+
+class FlatDiscount extends DiscountStrategy {
+    constructor(discountAmount) {
+        super();
+        this.discountAmount = discountAmount;
+    }
+
+    applyDiscount(price) {
+        return price - this.discountAmount;
+    }
+}
+
+class PercentageDiscount extends DiscountStrategy {
+    constructor(discountPercentage) {
+        super();
+        this.discountPercentage = discountPercentage;
+    }
+
+    applyDiscount(price) {
+        return price - price * (this.discountPercentage / 100);
+    }
+}
+
+// BigProduct Class
 class BigProduct {
-    constructor(serial, color, size, price, hasDiscount) {
+    constructor(serial, color, size, price, discountStrategy = new DiscountStrategy()) {
         this.serial = `#${serial}`;
         this.color = color;
         this.size = size;
-        this.price = hasDiscount ? price - 100 : price; // Apply discount if applicable
-        this.hasDiscount = hasDiscount;
+        this.price = discountStrategy.applyDiscount(price); // Apply selected discount strategy
+        this.discountStrategy = discountStrategy;
     }
 
     // Method to display discount message
     showMsg() {
-        return `You${this.hasDiscount ? "" : " Don't"} Have a Discount`;
+        return this.discountStrategy instanceof DiscountStrategy && !(this.discountStrategy instanceof FlatDiscount || this.discountStrategy instanceof PercentageDiscount)
+            ? "You Don't Have a Discount"
+            : "You Have a Discount";
     }
 }
 
 // Array to hold multiple products
 let products = [
-    new BigProduct(8722631, "red", "xl", 500, true),
-    new BigProduct(1234567, "blue", "m", 500, false),
-    new BigProduct(9876543, "green", "l", 500, true)
+    new BigProduct(8722631, "red", "xl", 500, new FlatDiscount(100)), // Flat discount
+    new BigProduct(1234567, "blue", "m", 500), // No discount
+    new BigProduct(9876543, "green", "l", 500, new PercentageDiscount(20)) // Percentage discount
 ];
 
 let finalData = "";
